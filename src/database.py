c8a21e57e3acd2d3c0f5b2044ebb67613e3d687e
unweightedai <welloss3000@gmail.com>
2024-12-21 19:33:47 -0800

    Handles KOL data

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import config
 from datetime import datetime, timedelta
-
+#bang 
 class Database:
     def __init__(self):
         self.client = MongoClient(config.MONGODB_URI)
